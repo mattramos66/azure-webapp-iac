b532a8b437edab19e2ec54526e6266384b3f9e89
mattramos66 <mattramos6@gmail.com>
2025-01-24 17:45:25 -0800

    Create AzureIaCWebapp.jsx

---
+++ AzureIaCWebapp.jsx
@@ -0,0 +1,7 @@
+export default function AzureIaCWebapp() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-green-500 text-4xl font-bold">IaC Deployable Webapp</h1>
+    </div>
+  );
+}
